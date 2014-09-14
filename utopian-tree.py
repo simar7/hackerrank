1296472e3217bd4409c36fc53d4886749b2d0063
Simarpreet Singh <s244sing@uwaterloo.ca>
2014-09-14 01:45:59 -0400

    WIP: ch2
    Signed-off-by: Simarpreet Singh <s244sing@uwaterloo.ca>

--- utopian-tree.py
+++ utopian-tree.py
@@ -4,20 +4,44 @@
 # T lines after 1st = N = # of cycles for that test case.
 
 '''
-    Cycles      ||      Length (origLength = 1)
-    0                   (origLength)
-    1                   (origLength*2)
-    2                   (origLength*2 + 1)
-    3                   (((origLength*2)+1)*2)
+    Cycles      ||      Length (origLength = 1)		||		output
+    0                   (origLength)							1
+    1                   (origLength*2)							2
+    2                   (origLength*2 + 1)						3
+    3                   (((origLength*2)+1)*2)					6
+    4					((((origLength*2)+1)*2)+1)				7
 '''
 import sys
 
+origLength = 1
+growthLength = 1
+
+# This method returns growth.
+def eval_height(cyclecount, growthLength):
+		
+	if int(cyclecount) == 0:
+		return int(growthLength)
+
+	elif int(cyclecount) & 0x1 == True: # Odd
+		return eval_height(int(cyclecount) - 1, growthLength * 2)
+	
+	elif int(cyclecount) & 0x1 == False: # Even
+		return eval_height(int(cyclecount) - 1, growthLength + 1)
+
+
 def main():
 	    
+ input_array = []
+    
+ for line in sys.stdin:
+ 	input_array.append(line)
 
+ num_of_test_cases = input_array[0]
 
+ for tree_cycle_count in input_array[1:]:
+ 	print (int(eval_height(tree_cycle_count, origLength)))
 
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
